7141102225376b2fa69fcf7188bfd05d0952edf8
Gnailimixam <max.gossler@web.de>
2018-11-21 12:57:50 +0100

    bla

--- js/corpus.js
+++ js/corpus.js
@@ -50,14 +50,17 @@ var labels = [
 corpus = {};
 corpus.who = [
 	"speaking AI" , //"Eine sprechende KI"
+	"amusing intelligence",
+	"intelligent assistant",
+	"personal intelligent assistant",
 	"logical intelligence" ,
 	"intelligent robot" ,
 	"observing AI" ,  //= visual-spatial
+	"detecting AI", //= visual-spatial
 	"social intelligence" ,
 	"social AI",
 	"ethical AI",
 	"emotional intelligence",
-	"intelligent assistant",
 	"man-machine cooperation",
 	"man-AI collaboration"
 		]
@@ -70,35 +73,68 @@ corpus.context = [  // Wishes? Forsights
 //	"during sports",
 	"during your leisure time",
 	"in your household",
+	"at housekeeping",
 	"in public",
 	"at university",
 	"at evening school",
 	"at your human intelligence jobs",
-	"during the holidays"
+	"during the holidays",
+	"in your health care",
+	"with heavy work"
 ]
 
 corpus.verb = [
-	"help you" , //"Ihnen in der Öffentlichkeit helfen"
 	"empower you",
-	"give you more free time" ,
-	"need your creativity" ,
-	"replace you" ,
-	"do the boring stuff" ,
-	"encourage you to be more creative" ,
-	"encourage you to do more things voluntarily" ,
-	"enhance your human power",
-	"transport you",
-	"make you become healthier",
-	"teach you new stuff",
-	"manage your finances",
-	"entertain you",
-	"save you time",
-	"save you money",
-	"organize your tasks",
+	"strengthen you",
+	"help you",
+	"enhance your creativity",
+	"need your creativity",
+
 	"provide you essential infos",
-	"instruct you",
+	"give you informations",
+	"give you fascinating insights",
+
 	"help you make the right decisions",
-	"enhance your creativity"
+	"support you to decide",
+
+	"save you time",
+	"give you more free time" ,
+	"do the boring stuff" ,
+
+	"save you money",
+	"negotiate the best for you",
+
+	"make you become healthier",
+
+	"teach you new stuff",
+	"instruct you",
+
+	"transport you",
+
+	"make you more money",
+
+	"organize your tasks",
+
+	"manage your finances",
+
+	"entertain you",
+
+	"solve your problems",
+	"do what's good for you",
+
+	"organize your stuff",
+
+	"do your jobs",
+
+	"be your friend",
+
+	"encourage you to be more creative",
+	"encourage you to do things voluntarily",
+
+	"replace you",
+	"be better than you",
+	"tell you what's important",
+	"tell you what's right"
 ]
 
 corpus.adjective = [
